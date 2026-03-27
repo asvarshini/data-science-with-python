ba3e3f6d82a305e2d377891b15fb3720e0a1fa6b
VarshiniAS <asvarshini84@gmail.com>
2026-03-27 21:05:42 +0530

    added how to import file

---
+++ Python_assignment/oop_assignment/import/module.py
@@ -0,0 +1,17 @@
+def add( a, b):
+        return a + b
+
+# Subtraction
+def sub( a, b):
+    return a - b
+
+# Multiplication
+def mul( a, b):
+    return a * b
+
+# Division
+def divi( a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
